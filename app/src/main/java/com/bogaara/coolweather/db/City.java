6b2802349df47abe40cd7082a3e03a7fc48de149
谭安溪 <tan_anxi@163.com>
2017-04-06 16:02:01 +0800

    加入加入数据库和表的各项配置

---
+++ app/src/main/java/com/bogaara/coolweather/db/City.java
@@ -0,0 +1,48 @@
+package com.bogaara.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by donghong on 17/4/6.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    private int provinceId;
+
+}
